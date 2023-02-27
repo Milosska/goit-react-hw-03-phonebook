de667fb33ed263512ffb69ebd12ef17859022c3f
Milosska <110258611+Milosska@users.noreply.github.com>
2023-02-27 23:32:42 +0200

    task is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,11 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
+const LS_KEY = 'contacts';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -62,6 +59,24 @@ export class App extends Component {
       return name.toLowerCase().includes(this.state.filter.toLowerCase());
     });
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem(LS_KEY);
+
+    savedContacts !== null
+      ? this.setState({
+          contacts: JSON.parse(savedContacts),
+        })
+      : this.setState({
+          contacts: [],
+        });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     return (
